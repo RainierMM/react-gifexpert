95d0c61efd7c9469c7622e16afd22c5f9237a54d
Rainier <rainiermichelena@gmail.com>
2021-02-21 18:44:25 -0500

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["Dragon Ball"]);
+
+  //   const handleAdd = () => {
+  //     // setCategories([...categories, "Pokemon"]);
+  //     setCategories((cats) => [...cats, "Pokemon"]);
+  //   };
+
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+
+      <ol>
+        {categories.map((category) => (
+          <GifGrid category={category} key={category} />
+        ))}
+      </ol>
+    </>
+  );
+};
